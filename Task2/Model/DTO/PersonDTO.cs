83114dced529d6a924669697d4809f7575ee65a7
Jamoliddin <100786889+Jamoliddin0710@users.noreply.github.com>
2023-10-16 20:03:18 +0500

    bug-fixed

--- Task2/Model/DTO/PersonDTO.cs
+++ Task2/Model/DTO/PersonDTO.cs
@@ -2,5 +2,11 @@ namespace Task2.Model.DTO;
 
 public class PersonDTO
 {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Patronymic { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string Age { get; set; }
+    public string PasportSeriaNumber { get; set; }
+    public string PNFL { get; set; }
 }
